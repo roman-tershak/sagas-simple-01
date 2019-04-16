f0333ac7618a007bff00528226338bc783b22758
Roman Tershak <roman.tershak@fluke.com>
2019-04-16 12:44:17 +0300

    changed exception handling to use Controller Advice

--- order-service/src/main/java/rt/sagas/orderservice/controllers/OrderController.java
+++ order-service/src/main/java/rt/sagas/orderservice/controllers/OrderController.java
@@ -1,11 +1,8 @@
 package rt.sagas.orderservice.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import rt.sagas.orderservice.entities.Order;
 import rt.sagas.orderservice.services.OrderService;
@@ -23,31 +20,6 @@ public class OrderController {
     @ResponseBody
     public Order create(@RequestBody @Valid Order order) {
         return orderService.createOrder(order);
-    }
-
-
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public ResponseEntity<ApiError> handleException(MethodArgumentNotValidException e) {
-        return getApiErrorResponseEntity(
-                e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
-                HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler({Exception.class})
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
-    public ResponseEntity<ApiError> handleException(Exception e) {
-        return getApiErrorResponseEntity(
-                "Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    private ResponseEntity<ApiError> getApiErrorResponseEntity(String errorMessage, HttpStatus status) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        return new ResponseEntity<>(
-                new ApiError(errorMessage), httpHeaders, status);
     }
 
 }
