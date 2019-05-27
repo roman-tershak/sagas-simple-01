ce881eeebce1c86c6d917b73bec5dccd267b01b6
Roman Tershak <roman.tershak@fluke.com>
2019-05-27 14:32:58 +0300

    parametrized test-loader

--- test-loader/test-loader-run.cmd
+++ test-loader/test-loader-run.cmd
@@ -1,1 +1,5 @@
-docker run -ti --rm --name test-loader --network vnet-saga-simple-1 rtershak/test-loader:latest
+docker run --name test-loader ^
+    -ti --rm ^
+    --network docker_vnet-saga-simple-1 ^
+    -e CONCURRENCY=10 -e HOLD_FOR=300s -e RAMP_UP=10s ^
+    rtershak/test-loader:latest
