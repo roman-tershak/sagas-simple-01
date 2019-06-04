efdf4e06d81523d8ab157a3d1f16aabff849c565
Roman Tershak <roman.tershak@fluke.com>
2019-06-04 19:37:20 +0300

    working at last

--- reservation-service/src/test/java/rt/sagas/reservation/TestConfiguration.java
+++ reservation-service/src/test/java/rt/sagas/reservation/TestConfiguration.java
@@ -1,8 +1,5 @@
 package rt.sagas.reservation;
 
-import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,18 +14,5 @@ public class TestConfiguration {
     @Bean
     public ReservationRepositorySpy reservationRepositorySpy(ReservationRepository reservationRepository) {
         return new ReservationRepositorySpy(reservationRepository);
-    }
-
-    @Bean
-    @ConditionalOnBean(ActiveMQConnectionFactory.class)
-    public InitializingBean jmsConnectionFactory(ActiveMQConnectionFactory connectionFactory) {
-        return configureRedeliveryPolicy(connectionFactory);
-    }
-
-    private InitializingBean configureRedeliveryPolicy(ActiveMQConnectionFactory connectionFactory) {
-        return () ->
-        {
-            connectionFactory.setRetryInterval(1000000L);
-        };
     }
 }
