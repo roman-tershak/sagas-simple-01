a76d5f9ccd027c3124cda5e97e7afe1544e65c5a
Roman Tershak <romantershak@rewconsultingservices.com>
2020-03-26 22:15:14 +0200

    some changes

--- test-loader/test-loader-from-mvn-run.cmd
+++ test-loader/test-loader-from-mvn-run.cmd
@@ -1,5 +1,5 @@
 docker run --name test-loader ^
     -ti --rm ^
     --network docker_vnet-saga-simple-1 ^
-    -e CONCURRENCY=10 -e HOLD_FOR=300s -e RAMP_UP=10s ^
+    -e CONCURRENCY=10 -e HOLD_FOR=900s -e RAMP_UP=10s ^
     saga-tests-simple-1/test-loader:1.0-ONE-COM-TRAN
